7049c274b46c999fdaaf6e0d3e4136d4b3573401
Kristof Zerbe <kristof.zerbe@gmail.com>
2019-09-30 14:51:30 +0200

    Parameters, new RunCommands method, new commands

---
+++ Options.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexoCommander
+{
+    public class Options
+    {
+        public Options(string[] args) {
+            foreach (string a in args)
+            {
+                var arg = a.Split('=');
+                switch (arg[0].ToUpper())
+                {
+                    case "/WORKDIR":
+                        WorkDir = arg[1];
+                        break;
+                }
+            }
+        }
+
+        public string WorkDir { get; set; } = Environment.CurrentDirectory;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,102 +2,136 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace HexoCommander
 {
     class Program
     {
+        /*  HexoCommander
+         *  ============
+         *  Parameters:
+         *  - /workdir:<Path>
+         */
+
         static void Main(string[] args)
         {
+            var opt = new Options(args);
+
             var _commandFile = "hexo-commands.txt";
 
-            /*  AVAILABLE COMMANDS:
-             *  -------------------
-             *  generate 
-             *  => generate only
-             *  
-             *  publish 
-             *  => generate, stage docs, commit & push
-             *  
-             *  PLANNED COMMANDS:
-             *  -----------------
-             *  publish-draft "<filename>"  
-             *  => publish draft       
-             * 
-             */
+            var _run_hexo_generate  = "hexo generate";
+            var _run_hexo_publish   = "hexo generate \"@title@\"";
+            var _run_git_stage      = "git add \"docs/*\"";
+            var _run_git_commit     = "git commit -m \"Remote publication via HexoCommander\"";
+            var _run_git_push       = "git push origin master";
 
-            var _cmd_hexo_generate  = "hexo generate";
-            var _cmd_git_stage      = "git add \"docs/*\"";
-            var _cmd_git_commit     = "git commit -m \"Remote publication via HexoCommander\"";
-            var _cmd_git_push       = "git push origin master";
+            var di = new DirectoryInfo(opt.WorkDir);
+            var fis = di.GetFiles(_commandFile);
 
-            var fi = new FileInfo(_commandFile);
-            if (fi.Exists)
+            if (fis.Length == 0)
             {
-                var cmds = File.ReadAllLines(fi.FullName);
+                Console.WriteLine(_commandFile + " not found");
+            }
+            else
+            {
+                var fi = fis[0];
+                var lns = File.ReadAllLines(fi.FullName);
                 var lst = new List<String>();
-                foreach (string s in cmds)
+                var cmdCount = 0;
+
+                foreach (string s in lns)
                 {
-                    var cmd = s.Split(':');
-                    if (cmd.Length > 1)
-                    {
+                    var ln = s.Split('@');
+                    if (ln.Length > 1) // already executed
+                    { 
                         lst.Add(s);
                     }
                     else
                     {
-                        Console.WriteLine("Do Command " + s.ToUpper());
+                        //separate command from parameters by colon
+                        var arr = s.Split(':');
+                        var cmd = arr[0].Trim().ToUpper();
 
-                        switch (s.ToUpper())
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("HexoCommander is running " + cmd);
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        var cmds = new List<string>();
+
+                        switch (cmd)
                         {
                             case "GENERATE":
-                                Console.WriteLine(RunCommand(_cmd_hexo_generate));
+                                cmds.Add(_run_hexo_generate);
+                                RunCommands(cmds, opt.WorkDir);
+                                break;
+
+                            case "POSTDRAFT":
+                                cmds.Add(_run_hexo_publish.Replace("@title@", arr[1])); // parameter 1 expects title to publish
+                                cmds.Add(_run_hexo_generate);
+                                RunCommands(cmds, opt.WorkDir);
                                 break;
 
                             case "PUBLISH":
-                                Console.WriteLine(RunCommand(_cmd_hexo_generate));
-                                Console.WriteLine(RunCommand(_cmd_git_stage));
-                                Console.WriteLine(RunCommand(_cmd_git_commit));
-                                Console.WriteLine(RunCommand(_cmd_git_push));
+                                cmds.Add(_run_hexo_generate);
+                                cmds.Add(_run_git_stage);
+                                cmds.Add(_run_git_commit);
+                                cmds.Add(_run_git_push);
+                                RunCommands(cmds, opt.WorkDir);
                                 break;
+
                             default:
                                 Console.WriteLine("Unknown Command");
                                 break;
                         }
-                        lst.Add(s + ": " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                        lst.Add(s + " @ " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+                        cmdCount += 1;
                     }
                 }
                 File.WriteAllLines(fi.FullName, lst);
-            }
-            else {
-                Console.WriteLine(_commandFile + " not found");
+
+                if (cmdCount == 0)
+                {
+                    Console.WriteLine("There was nothing to do...");
+                }
             }
 
-            // comment out for release
-            //Console.ReadLine();
+            // comment in for debugging
+            Console.ReadLine();
         }
 
-        private static string RunCommand(string cmd)
+        static void RunCommands(List<string> cmds, string workingDirectory)
         {
-            Process proc = new Process();
-
-            proc.StartInfo.FileName = "cmd.exe";
-            proc.StartInfo.RedirectStandardInput = true;
-            proc.StartInfo.RedirectStandardOutput = true;
-            proc.StartInfo.CreateNoWindow = true;
-            proc.StartInfo.UseShellExecute = false;
-
+            var proc = new Process();
+            var psi = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                WorkingDirectory = workingDirectory,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            };
+            proc.StartInfo = psi;
             proc.Start();
 
-            proc.StandardInput.WriteLine(cmd);
-            proc.StandardInput.Flush();
-            proc.StandardInput.Close();
+            proc.OutputDataReceived += (sender, e) => { Console.WriteLine(e.Data); };
+            proc.ErrorDataReceived += (sender, e) => { Console.WriteLine(e.Data); };
+            proc.BeginOutputReadLine();
+            proc.BeginErrorReadLine();
 
-            var ret = proc.StandardOutput.ReadToEnd();
-
-            proc.Close();
-
-            return ret;
+            using (StreamWriter sw = proc.StandardInput)
+            {
+                foreach (var cmd in cmds)
+                {
+                    sw.WriteLine(cmd);
+                }
+            }
+            proc.WaitForExit();
         }
+
     }
 }
