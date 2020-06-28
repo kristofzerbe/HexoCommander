33de49f1264bfb8ec645fa608d3636860f790bb5
Kristof Zerbe <kristof.zerbe@gmail.com>
2020-06-28 13:19:06 +0200

    Updated AssemblyVersion structure

--- AssemblyVersion/CreateBuildLog.bat
+++ AssemblyVersion/CreateBuildLog.bat
@@ -1,5 +1,5 @@
 @echo off
-echo PostBuild: Doing BuildLog
+echo CreateBuildLog...
 
 set PROJECT_DIR=%1
 set VERSION=%2
@@ -12,7 +12,7 @@ FOR /f "tokens=1,2,3,4 delims=." %%a IN ("%VERSION%") do (
 )
 
 set BUILDLOG_FILE=%MAJOR%.%MINOR%.log
-set BUILDLOG_FOLDER=%PROJECT_DIR%\BuildLogs
+set BUILDLOG_FOLDER=%PROJECT_DIR%\AssemblyVersion\BuildLogs
 
 if not exist %BUILDLOG_FOLDER% mkdir %BUILDLOG_FOLDER%
 
